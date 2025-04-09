a0c4d19776f08e6168797719b6e8519003e9f3e2
tenhou-Ravenclaw <tenhou.0126@gmail.com>
2025-04-09 23:43:00 +0900

    station13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,11 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
+  const isChecked = e.target.checked;
+  const textElement = document.getElementById("text");
 
+  if (isChecked) {
+    textElement.style.backgroundColor = "red"; // 好きな色に変更
+  } else {
+    textElement.style.backgroundColor = "transparent"; // 元に戻す
+  }
 }
